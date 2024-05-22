ff716c11a842e7915910ca9448230c8baf43d1d7
Roshan Jain <roshanjain.220407@gmail.com>
2024-05-22 11:43:15 +0530

    Step 1

---
+++ script.js
@@ -0,0 +1,22 @@
+var h1 = document.querySelector("h1");
+var text = "FORD";
+var arr = text.split("");
+
+function generateText(text) {
+  text.forEach((data) => {
+    var span = document.createElement("span");
+    span.classList.add("char");
+    span.innerHTML = data;
+    h1.appendChild(span);
+  });
+}
+generateText(arr);
+// gsap
+gsap.from(".char", {
+  y: 100,
+  stagger: 0.13,
+  opacity: 0,
+  delay: 0.1,
+  ease: "ease-in",
+  duration: 1,
+});
