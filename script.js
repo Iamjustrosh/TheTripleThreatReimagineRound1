79498bbc9eced864bdcb6809ec8b95f8095f8f4c
Roshan Jain <roshanjain.220407@gmail.com>
2024-05-23 17:51:19 +0530

    Minor Changes

--- script.js
+++ script.js
@@ -1,22 +1,106 @@
-var h1 = document.querySelector("h1");
-var text = "Ford";
+function aniSplashScreen(){
+  var h1 = document.querySelector(".splLoad");
+  var text = "Ford";
+  var arr = text.split("");
+  
+  function generateText(text) {
+    text.forEach((data) => {
+      var span = document.createElement("span");
+      span.classList.add("char");
+      span.innerHTML = data;
+      h1.appendChild(span);
+    });
+  }
+  generateText(arr);
+  // gsap
+  gsap.from(".char", {
+    y: 100,
+    stagger: 0.13,
+    opacity: 0,
+    delay: 0.1,
+    ease: "ease-in",
+    duration: 1,
+  });
+}
+
+
+gsap.from(".heroTxt", {
+  y: 100,
+  stagger: 0.10,
+  opacity: 0.5,
+  delay: 3.1,
+  ease: "ease-in",
+  duration: 0.8,
+});
+
+function cursor(){
+
+  var main = document.querySelector("#main");
+  var cursor = document.querySelector("#cursor"); 
+  main.addEventListener("mousemove",function(pts){
+    gsap.to(cursor,{
+      x:pts.x,
+      y:pts.y,
+    })
+  })
+
+
+  var mustang = document.querySelector("#car");
+  mustang.addEventListener("mouseenter",function(){
+    cursor.innerHTML ="Explore";
+    gsap.to(cursor,{
+      scale: "4",
+      backgroundColor: "#f35f5fd8 "
+    })
+  })
+  mustang.addEventListener("mouseleave",function(){
+    cursor.innerHTML =""
+    gsap.to(cursor,{
+      scale: "1",
+      backgroundColor: "#ffffff93"
+    })
+  })
+
+}
+
+
+function aniCar(){
+  var car = document.querySelector("#car");
+  gsap.from(car,{
+    x: 200,
+    delay: 3,
+    ease: "ease-in",
+    duration: 1.2,
+  })
+}
+
+function heroTxt(){
+var h1 = document.querySelector(".heroTxt");
+var text = "MUSTANG";
 var arr = text.split("");
 
 function generateText(text) {
   text.forEach((data) => {
     var span = document.createElement("span");
-    span.classList.add("char");
+    span.classList.add("chr");
     span.innerHTML = data;
     h1.appendChild(span);
   });
 }
 generateText(arr);
 // gsap
-gsap.from(".char", {
+gsap.from(".chr", {
   y: 100,
   stagger: 0.13,
   opacity: 0,
-  delay: 0.1,
+  delay: 3.1,
   ease: "ease-in",
   duration: 1,
 });
+}
+
+
+aniCar();
+cursor();
+aniSplashScreen();
+heroTxt();
